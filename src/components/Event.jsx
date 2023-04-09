6588fc2715f89468949f5023805ef2e67ef9910f
unknown <maretanur197@gmail.com>
2023-04-09 21:50:22 +0700

    update event and faq

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,53 +1,117 @@
-import React, {useState} from 'react'
-import data from '../components/data'
+import React, { useState } from "react";
+import data from "../components/data";
 //import pik from '../assets/support.jpg'
 //import {motion} from 'framer-motion';
 
 const Event = () => {
-    const [count, setCount] = useState(3)
-    const [noOfElement, setnoOfElement] = useState(3);
+  const [count, setCount] = useState(3);
+  const [noOfElement, setnoOfElement] = useState(3);
 
-    const loadMore = () => {
-        setCount(6)
-    }
-    console.log(data.cardData) 
-    // const slice = data.cardData.slice(0, noOfElement);
+  const loadMore = () => {
+    setCount(6);
+  };
+  console.log(data.cardData);
+  // const slice = data.cardData.slice(0, noOfElement);
 
-   return (
-    <div className='max-w-[1240px] px-5 mx-auto items-center'>
-      <h1 className='pb-5 pt-28 text-5xl font-bold text-center'>Event</h1>
-      <div className='flex mx-auto justify-between'> 
-      <div className='grid sm:grid-cols-2 md:grid-cols-3 pt-2 gap-4 items-center' >  
-      {data.cardData.map((item, index) => {
-        if( index < count){
-            return (
-            <div  class="max-w-[1240px] border border-gray-200 rounded-lg shadow overflow-hidden" key={index}>            
-                <a href="#">
+  const scrollUp = () => {
+    setCount(3);
+  };
+  console.log(data.cardData);
+
+  return (
+    <div name="event" className="max-w-[1240px] px-5 mx-auto items-center">
+      <h1 className="pb-5 pt-28 text-5xl font-bold text-center">Event</h1>
+      <div className="flex mx-auto justify-between">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 pt-2 gap-4 items-center">
+          {data.cardData.map((item, index) => {
+            if (index < count) {
+              return (
+                <div
+                  class="max-w-[1240px] border border-gray-200 rounded-lg shadow overflow-hidden"
+                  key={index}
+                >
+                  <a href="#">
                     <img class="rounded-t-lg" src={item.img} alt="" />
-                </a>
-                <div class="p-5">
+                  </a>
+                  <div class="p-5">
                     <a href="#">
-                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-green-500">{item.title}</h5>
+                      <h5 class="mb-2 text-2xl font-bold tracking-tight text-black">
+                        {item.title}
+                      </h5>
                     </a>
-                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{item.desc}</p>
-                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> {item.button}                         
-                    <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg></a>
+                    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                      {item.desc}
+                    </p>
+                    <a
+                      href="#"
+                      class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                      {" "}
+                      {item.button}
+                      <svg
+                        aria-hidden="true"
+                        class="w-4 h-4 ml-2 -mr-1"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </a>
+                  </div>
                 </div>
-           </div>       
-          )
-                    
-        }
-          }
-)}                
-        </div>    
+              );
+            }
+          })}
         </div>
-        <div className='pt-10'>
-            <button className='btn-more py-3 px-10 grid justify-items-center mx-auto' onClick={() => loadMore()}>  Load More
-            </button> 
-        </div>         
-        </div>
-            
-  )
-}
+      </div>
+      <div className="pt-10 flex flex-row gap-2 items-end justify-end">
+        <button
+          className="rounded-sm h-7 w-8 px-2 items-center text-center "
+          onClick={() => loadMore()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </button>
+
+        <button
+          className="rounded-sm h-7 w-8 px-2 items-center justify-center text-center"
+          onClick={() => scrollUp()}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M4.5 15.75l7.5-7.5 7.5 7.5"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Event;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,100 +1,143 @@
-import React from 'react'
+import React from "react";
 import { useSpring, animated } from "react-spring";
 import { motion } from "framer-motion";
 
 import {
-    CloudUploadIcon,
-    DatabaseIcon,
-    PaperAirplaneIcon,
-    ServerIcon,
-} from '@heroicons/react/solid'
+  CloudUploadIcon,
+  DatabaseIcon,
+  PaperAirplaneIcon,
+  ServerIcon,
+} from "@heroicons/react/solid";
 
-import bgImg from '../assets/cek.png'
+import bgImg from "../assets/cek.png";
 
-let easeing = [0.6,-0.05,0.01,0.99];
+let easeing = [0.6, -0.05, 0.01, 0.99];
 const stagger = {
-  animate:{
-    transition:{
-      delayChildren:0.4,
-      staggerChildren:0.2,
-      staggerDirection:1
-    }
-  }
-}
-const header={
-    initial:{
-      y:-60,
-      opacity:0,
-      transition:{duration:0.05, ease:easeing}
+  animate: {
+    transition: {
+      delayChildren: 0.4,
+      staggerChildren: 0.2,
+      staggerDirection: 1,
     },
-    animate:{
-      y:0,
-      opacity:1,
-      animation:{
-        duration:0.6,
-        ease:easeing
-      }
-    }
-  };
-  const fadeInUp = {
-    initial:{
-      y:-60,
-      opacity:0,
-      transition:{
-        duration:0.6, ease:easeing
-      }
+  },
+};
+const header = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: { duration: 0.05, ease: easeing },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    animation: {
+      duration: 0.6,
+      ease: easeing,
     },
-    animate:{
-      y:0,
-      opacity:1,
-      transition:{
-        duration:0.6,
-        delay:0.5,
-        ease:easeing
-      }
-    }
-  };
-  
-  
+  },
+};
+const fadeInUp = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+      ease: easeing,
+    },
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      delay: 0.5,
+      ease: easeing,
+    },
+  },
+};
 
-function Number ({n}) {
-    const {number} = useSpring({
-        from : {number : 0}, 
-        number : n,
-        delay : 500,
-        config : {mass: 1, tension: 20, friction: 10 }, 
-    });
-    return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>
+function Number({ n }) {
+  const { number } = useSpring({
+    from: { number: 0 },
+    number: n,
+    delay: 500,
+    config: { mass: 1, tension: 20, friction: 10 },
+  });
+  return <animated.div>{number.to((n) => n.toFixed(0))}</animated.div>;
 }
 
 const Hero = () => {
   return (
-    <div name='home' className='bg-zinc-200 pt-20 md:pt-12'>
-        <div className='grid md:grid-cols-2 max-w-7xl md:mx-auto mx-5'>
-            <div className='flex flex-col justify-center sm:items- w-full px-2 py-8'>
-                <div data-aos="fade-right" className='py-3 text-5xl md:text-6xl font-bold text-center md:text-left'>Perhimpunan Ahli Kesehatan Kerja Indonesia</div>
-                <div className='grid grid-cols-1 justify-items-center sm:justify-items-start md:pt-3' >
-                    <button data-aos="zoom-in" className='bg-green-500 border-green-500 md: py-3 md:px-16 px-9'>Daftar</button>
-                </div>
-            </div>
-            <div data-aos="fade-left">
-                <img className='xl:w-full md:pt-24' src={bgImg} alt="/" />
-            </div>
-            <div className='grid grid-cols-1 justify-items-center z-10 mt-[-4rem]'>
-                <div className='container items-center grid grid-cols-1 md:max-w-6xl absolute py-8
-                mx-6 md:left-1/2 transform md:-translate-x-1/2 md:mx-auto bg-zinc-200
-                border border-slate-300 rounded-xl text-center shadow-xl'>
-                    <div className='flex justify-between flex-wrap mx-3'>
-                        <div><p className='pl-6'><CloudUploadIcon className='h-6 text-green-500' /></p><div className='md:text-3xl text-xl font-bold'><Number n={67}/></div><p className='md:px-4 px-2 py- text-slate-500'>Daerah</p></div>
-                        <div><p className='pl-7'><CloudUploadIcon className='h-6 text-green-500' /></p><div className='md:text-3xl text-xl font-bold'><Number n={230}/></div><p className='md:px-4 px-2 py- text-slate-500'>Cabang</p></div>
-                        <div><p className='pl-6'><CloudUploadIcon className='h-6 text-green-500' /></p><div className='md:text-3xl text-xl font-bold'><Number n={576}/></div><p className='md:px-4 px-2 py- text-slate-500'>Ranting</p></div>
-                        <div><p className='pl-7'><CloudUploadIcon className='h-6 text-green-500' /></p><div className='md:text-3xl  text-xl font-bold'><Number n={7089}/></div><p className='md:px-4 px-2 py- text-slate-500'>Anggota</p></div>
-                    </div>
-                </div>
-            </div>
+    <div name="home" className="bg-zinc-200 pt-20 md:pt-12">
+      <div className="grid md:grid-cols-2 max-w-7xl md:mx-auto mx-5">
+        <div className="flex flex-col justify-center sm:items-cente w-full px-2 py-8">
+          <div
+            data-aos="fade-right"
+            className="py-3 text-4xl md:text-6xl font-bold text-center md:text-left"
+          >
+            Perhimpunan Ahli Kesehatan Kerja Indonesia
+          </div>
+          <div className="grid grid-cols-1 justify-items-center sm:justify-items-start md:pt-3">
+            <button
+              data-aos="zoom-in"
+              className="bg-green-500 border-green-500 md: py-3 md:px-16 px-9"
+            >
+              Daftar
+            </button>
+          </div>
         </div>
+        <div data-aos="fade-left">
+          <img className="xl:w-full md:pt-24" src={bgImg} alt="/" />
+        </div>
+        <div className="grid grid-cols-1 justify-items-center z-10 mt-[-4rem]">
+          <div
+            className="container items-center grid grid-cols-1 md:max-w-6xl absolute py-8
+                mx-6 md:left-1/2 transform md:-translate-x-1/2 md:mx-auto bg-white
+                border border-slate-300 rounded-xl text-center shadow-xl"
+          >
+            <div className="flex justify-between flex-wrap mx-3">
+              <div>
+                <p className="pl-6">
+                  <CloudUploadIcon className="h-6 text-green-500" />
+                </p>
+                <div className="md:text-3xl text-xl font-bold">
+                  <Number n={67} />
+                </div>
+                <p className="md:px-4 px-2 py- text-slate-500">Daerah</p>
+              </div>
+              <div>
+                <p className="pl-7">
+                  <CloudUploadIcon className="h-6 text-green-500" />
+                </p>
+                <div className="md:text-3xl text-xl font-bold">
+                  <Number n={230} />
+                </div>
+                <p className="md:px-4 px-2 py- text-slate-500">Cabang</p>
+              </div>
+              <div>
+                <p className="pl-6">
+                  <CloudUploadIcon className="h-6 text-green-500" />
+                </p>
+                <div className="md:text-3xl text-xl font-bold">
+                  <Number n={576} />
+                </div>
+                <p className="md:px-4 px-2 py- text-slate-500">Ranting</p>
+              </div>
+              <div>
+                <p className="pl-7">
+                  <CloudUploadIcon className="h-6 text-green-500" />
+                </p>
+                <div className="md:text-3xl  text-xl font-bold">
+                  <Number n={7089} />
+                </div>
+                <p className="md:px-4 px-2 py- text-slate-500">Anggota</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
